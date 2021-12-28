0584b74a08ebaf9f13577661120d4896a23c286f
JP Moresmau <jp@moresmau.fr>
2021-12-28 17:15:44 +0100

    daemon

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -e ROCKET_DATABASES="" -e AWS_ACCESS_KEY_ID= -e AWS_SECRET_ACCESS_KEY= -e AWS_DEFAULT_REGION=eu-west-1 -p 443:443 dataregi:0.0.1
+docker run -d -e ROCKET_DATABASES="" -e AWS_ACCESS_KEY_ID= -e AWS_SECRET_ACCESS_KEY= -e AWS_DEFAULT_REGION=eu-west-1 -p 443:443 dataregi:0.0.1
